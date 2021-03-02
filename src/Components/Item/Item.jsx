7d680cd0eb13f65b923ee8c86c4f5df4a6cb6079
Gluhenko Daniil <gluhenko-dv@yandex.ru>
2021-03-02 08:38:37 +0300

    add useCallback

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,10 +1,15 @@
 import {useDispatch} from 'react-redux';
+import React, {useCallback} from 'react';
 export const Item = ({id, text, isCompleted}) => {
     const dispatch = useDispatch();
+    const completeTask = useCallback(() => {
+        dispatch({type: 'COMPLETE_TASK', id: id});
+    }, [dispatch, id]);
+
     return (
         <div className={isCompleted ? `item item-completed` : `item`}>
             <input
-                onClick={() => dispatch({type: 'COMPLETE_TASK', id: id})}
+                onClick={completeTask}
                 className="custom-checkbox"
                 type="checkbox"
                 id={`item-${id}`}
--- src/Components/Item/editItem.jsx
+++ src/Components/Item/editItem.jsx
@@ -1,16 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import {useDispatch} from 'react-redux';
 import {DeleteBtn} from '../styled';
 import {Modal} from '../Modal/Modal';
 export const EditItem = (item) => {
     const dispatch = useDispatch();
+    const deleteTask = useCallback(() => {
+        dispatch({type: 'REMOVE_TASK', id: item.id});
+    }, [dispatch, item.id]);
     const [addModal, setAddModal] = useState(false);
     const toggleModal = () => {
         setAddModal(!addModal);
     };
     return (
         <div className={item.isCompleted ? `item item-completed` : `item`}>
-            <DeleteBtn onClick={() => dispatch({type: 'REMOVE_TASK', id: item.id})} />
+            <DeleteBtn onClick={deleteTask} />
             <span onClick={toggleModal} className="item-text">
                 {item.text}
             </span>
