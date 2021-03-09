a2c398dca93ddd1f95c8fc136b3c04025d27959b
Daniil Gluhenko <gluhenko-dv@yandex.ru>
2021-03-09 13:30:51 +0300

    save to localStorage

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,4 +1,4 @@
-const TASKS = [
+const defaultTasks = [
     {
         id: 1,
         text: `Пройти стажировку в Онли`,
@@ -26,10 +26,13 @@ const TASKS = [
     },
 ];
 
+localStorage.tasksList ? JSON.stringify(localStorage.tasksList) : JSON.stringify(defaultTasks);
+const TASKS = JSON.parse(localStorage.tasksList);
+
 const tasks = (state = TASKS, {id, text, isCompleted, type}) => {
     switch (type) {
         case 'ADD_TASK':
-            return [
+            state = [
                 ...state,
                 {
                     id,
@@ -37,18 +40,26 @@ const tasks = (state = TASKS, {id, text, isCompleted, type}) => {
                     isCompleted,
                 },
             ];
+            localStorage.tasksList = JSON.stringify(state);
+            return state;
         case 'COMPLETE_TASK':
-            return state.map((task) => {
+            state = state.map((task) => {
                 if (task.id === id) task.isCompleted = !task.isCompleted;
                 return task;
             });
+            localStorage.tasksList = JSON.stringify(state);
+            return state;
         case 'REMOVE_TASK':
-            return state.filter((task) => task.id !== id);
+            state = state.filter((task) => task.id !== id);
+            localStorage.tasksList = JSON.stringify(state);
+            return state;
         case 'EDIT_TASK':
-            return state.map((task) => {
+            state = state.map((task) => {
                 if (task.id === id) task.text = text;
                 return task;
             });
+            localStorage.tasksList = JSON.stringify(state);
+            return state;
         default:
             return state;
     }
